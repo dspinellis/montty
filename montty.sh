744c6a710756b487378b7a0a37fa355edd8df7ec
Diomidis Spinellis <dds@aueb.gr>
2016-07-11 00:37:08 +0300

    Set the port to a sane state

--- montty.sh
+++ montty.sh
@@ -1,26 +1,35 @@
 #!/bin/sh
 
 pidfiledir=/var/run
-DEV=cuaa0
+DEV=ttyACM0
 CMD1='AT\r'
 CMD2='ATZ\r'
 
+sanemode()
+{
+  stty -F /dev/$DEV cs8 115200 ignbrk -brkint -icrnl -imaxbel -opost \
+    -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke \
+    noflsh -ixon -crtscts
+}
+
 case "$1" in
 start)
-	/usr/home/dds/montty/montty $DEV $CMD1 $CMD2 $CMD3 && echo -n ' montty'
-	;;
+  sanemode
+  /usr/local/sbin/montty $DEV $CMD1 $CMD2 $CMD3 && echo -n ' montty'
+  ;;
 stop)
-	kill `cat $pidfiledir/montty.$DEV.pid`
-	;;
+  kill `cat $pidfiledir/montty.$DEV.pid`
+  ;;
 restart)
-	kill `cat $pidfiledir/montty.$DEV.pid`
-	rm -f /var/spool/lock/LCK..cuaa0
-	sleep 1
-	/usr/home/dds/montty/montty $DEV $CMD1 $CMD2 $CMD3 && echo -n ' montty'
-	;;
+  kill `cat $pidfiledir/montty.$DEV.pid`
+  rm -f /var/spool/lock/LCK..cuaa0
+  sleep 1
+  sanemode
+  /usr/home/dds/montty/montty $DEV $CMD1 $CMD2 $CMD3 && echo -n ' montty'
+  ;;
 *)
-	echo "Usage: `basename $0` {start|stop}" >&2
-	;;
+  echo "Usage: `basename $0` {start|stop}" >&2
+  ;;
 esac
 
 exit 0
